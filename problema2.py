e704fc192aa2930518b25b366f8052c991c0a890
kellytecnm <L24230550@minatitlan.tecnm.mx>
2024-12-13 22:02:56 -0600

    Create problema2.py

---
+++ problema2.py
@@ -0,0 +1,7 @@
+#Hacer un programa que sume dos números y muestre el resultado
+
+
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+suma = num1 + num2
+print("La suma es:", suma)
