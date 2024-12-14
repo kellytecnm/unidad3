da7e631a56a1c8def8eecca4fb0ba3b1b46fd543
kellytecnm <L24230550@minatitlan.tecnm.mx>
2024-12-13 22:03:42 -0600

    Create programa3.py

---
+++ programa3.py
@@ -0,0 +1,9 @@
+#Ingresar la nota del examen de programación y si es mayor o igual a siete imprimirque aprobó,
+# sino que debe recuperar.
+
+
+nota = int(input("Ingrese la nota del examen de programación: "))
+if nota >= 70:
+    print("Aprobó")
+else:
+    print("debe ir a complementario")
