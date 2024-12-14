1bd8337000c15534e69a096d39b1f3d8d476da58
kellytecnm <L24230550@minatitlan.tecnm.mx>
2024-12-13 22:05:01 -0600

    Create programa6.py

---
+++ programa6.py
@@ -0,0 +1,8 @@
+#Dados 3 números determinar el mayor e informar por pantalla el resultado
+
+
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+num3 = int(input("Ingrese el tercer número: "))
+mayor = max(num1, num2, num3)
+print("El número mayor es:", mayor)
